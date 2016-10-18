c7d45278a385ff6ab6aa63264b992e67647cc685
blakeFleck <fleckblake4@gmail.com>
2016-10-18 09:03:16 -0700

    finished getElement, now need to fix soemthing in stingify

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,39 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
-};
+var getElementsByClassName = function(className) {
+
+  var answer = [];
+
+  function recurse(elem) {
+
+    if( elem && elem.classList ){
+
+        var classList = Array.prototype.slice.call(elem.classList);
+
+        if (classList.includes(className)) {
+
+            answer.push(elem);
+         }
+      }
+
+        if (elem===false || elem.childNodes===false) {
+
+      return answer;
+
+    }
+
+    elem.childNodes.forEach(function(childNode) {
+
+       recurse(childNode);
+
+    });
+  }
+
+  recurse(document.body);
+  return answer;
+
+
+}
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,68 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
-};
+var stringifyJSON = function (answer){
+
+
+  if ( typeof answer === "number" || typeof answer === 'boolean' ){
+    return answer.toString();
+  }
+  if ( answer=== null ){
+    return "null";
+  }
+  if ( typeof answer === 'string' ){
+    return  '"'+answer+'"';
+  }
+  if( Array.isArray(answer) && answer.length === 0){
+    return '[]';
+  }
+
+
+  if ( Array.isArray(answer) && answer.length > 0 ){
+    var result = [];
+    for ( var i = 0; i < answer.length;i++ ){
+
+      result.push(stringifyJSON(answer[i]))
+
+    }
+    return '['+ result +']';
+  }
+
+  if( answer.constructor === Object && Object.keys(answer).length == 0){
+    return '{}';
+  }
+  if( answer.constructor === Object ){
+    var result = '';
+    var counter = 0;
+
+
+    for(var key in answer){
+      if(counter === Object.keys(answer).length-1){
+
+        result +=  stringifyJSON(key) + ":" + stringifyJSON(answer[key])
+        console.log('{' +  result + '}');
+
+        return '{' +  result + '}';
+
+      }
+
+
+
+     result +=  stringifyJSON(key) + ":" + stringifyJSON(answer[key]) + ','
+     counter++
+    }
+      // var resultTwo = result.slice(0, result.length-1)
+     return '{' +  result + '}'
+
+  }
+
+//   {"boolean, true": true, "boolean, false": false, "null": null },
+//   // basic nesting
+//   {"a":{"b":"c"}},
+//   {"a":["b", "c"]},
+//   [{"a":"b"}, {"c":"d"}],
+//   {"a":[],"c": {}, "b": true}
+// ];
+
+
+}
