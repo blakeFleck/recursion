b7e27572f65317365f7c999c98b21053c69d0d05
blakeFleck <fleckblake4@gmail.com>
2016-10-19 15:41:50 -0700

    changed stingify to work, had to get rid of .toString and .constructor

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,15 +5,17 @@
 
 var stringifyJSON = function (answer){
 
-
   if ( typeof answer === "number" || typeof answer === 'boolean' ){
-    return answer.toString();
+    var result = ""+answer;
+    return result;
   }
+
   if ( answer=== null ){
     return "null";
   }
   if ( typeof answer === 'string' ){
-    return  '"'+answer+'"';
+    var result = '"'+answer+'"';
+    return  result;
   }
   if( Array.isArray(answer) && answer.length === 0){
     return '[]';
@@ -30,19 +32,22 @@ var stringifyJSON = function (answer){
     return '['+ result +']';
   }
 
-  if( answer.constructor === Object && Object.keys(answer).length == 0){
+  if( typeof answer==="object"  && Object.keys(answer).length === 0){
     return '{}';
   }
-  if( answer.constructor === Object ){
+  if( typeof answer==="object" && Object.keys(answer).length>0){
     var result = '';
     var counter = 0;
 
 
     for(var key in answer){
+
+      if(answer[key]===undefined || typeof answer[key]==="function"){
+        break;
+      }
       if(counter === Object.keys(answer).length-1){
 
         result +=  stringifyJSON(key) + ":" + stringifyJSON(answer[key])
-        console.log('{' +  result + '}');
 
         return '{' +  result + '}';
 
